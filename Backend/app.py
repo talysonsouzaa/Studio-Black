d62d955c78c2baeeee64979f5525d2ce66e02ed8
Talyson Souza <Talysonsouzagomes10@gmail.com>
2026-06-14 15:01:20 -0300

    Redirecionando

--- Backend/app.py
+++ Backend/app.py
@@ -494,4 +494,6 @@ def admin_listar_bloqueios():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.getenv('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
