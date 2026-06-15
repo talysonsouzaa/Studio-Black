fc78e51c3c763812ad2f06d06185e8d46bfcec57
Talyson Souza <Talysonsouzagomes10@gmail.com>
2026-06-15 20:29:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import logging
 import os
 
 
-app = Flask(__name__, static_folder='docs', static_url_path='')
+app = Flask(__name__, static_folder='', static_url_path='')
 CORS(app)
 
 logging.basicConfig(level=logging.INFO)
